885aa7100b4fe55432cf62255e8da5a9d97838b8
OLD_RD6莊智強 <144870262+nukejhuang@users.noreply.github.com>
2024-03-01 17:28:32 +0800

    Update checkBuild.py

--- checkBuild.py
+++ checkBuild.py
@@ -9,16 +9,17 @@ if len(sys.argv) < 2:
     print("Usage %s <jenkins batch job name>" % sys.argv[0])
     sys.exit()
 
-JOB=sys.argv[1]
+#JOB=sys.argv[1]
 
-DIR = os.popen("getTriggerBuilds "+JOB).read()
+#DIR = os.popen("getTriggerBuilds "+JOB).read()
+DIR = 'Foo'
 FILENAME='Jenkins report at '+DIR
-HTML=DIR+'.html'
-ATT=DIR+'.zip'
+#HTML=DIR+'.html'
+#ATT=DIR+'.zip'
 output='No message'
 att=None
-if os.path.isfile(HTML):
-    output=open(HTML,'r').read()
-if os.path.isfile(ATT):
-    att=[ATT]
+#if os.path.isfile(HTML):
+#    output=open(HTML,'r').read()
+#if os.path.isfile(ATT):
+#    att=[ATT]
 mail.sendmail(FILENAME,output,att)
