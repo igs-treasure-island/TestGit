5aa373313ce7c18b8b2eaed988a4ef11074bc6a0
OLD_RD6莊智強 <144870262+nukejhuang@users.noreply.github.com>
2024-11-25 16:01:27 +0800

    Update db.go

--- db.go
+++ db.go
@@ -12,11 +12,16 @@ import (
 var (
 	/* 資料庫 */
 	backendDb *gorm.DB // 讀資料專用通道
+	logDb *gorm.DB // 讀資料專用通道
 )
 
 func dbInit() {
 	var err error
 	backendDb, err = gorm.Open(sqlserver.Open(db.Get("BackendDb").SQLServerDSN("backend")), &gorm.Config{QueryFields: true, SkipDefaultTransaction: true})
+	if err != nil {
+		panic(fmt.Sprintf("cannot create engine. ERR=%s", err.Error()))
+	}
+	logDb, err = gorm.Open(sqlserver.Open(db.Get("EventDb").SQLServerDSN("EventLog")), &gorm.Config{QueryFields: true, SkipDefaultTransaction: true})
 	if err != nil {
 		panic(fmt.Sprintf("cannot create engine. ERR=%s", err.Error()))
 	}
