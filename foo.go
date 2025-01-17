3812f3ea35ec3c90dd1e1a7626597d0b13aea254
OLD_RD6莊智強 <144870262+nukejhuang@users.noreply.github.com>
2025-01-17 14:11:57 +0800

    Update foo.go

--- foo.go
+++ foo.go
@@ -10,7 +10,18 @@ func bar() {
 	fmt.Println("bar")
 }
 
-func both() {
+func foo1() {
+	fmt.Println("foo")
+}
+
+func bar1() {
+	fmt.Println("bar")
+}
+
+
+func init() {
 	foo()
 	bar()
+	foo1()
+	bar1()
 }
